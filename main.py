d7136e4d6ea3edf097fa8cdfb56f91bb3e772cf1
Lucas Bordin <lucastrentinibordin@hotmail.com>
2023-10-22 03:39:05 -0300

    ajusta prints e comentários

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import psycopg2
 import json
 import re
+import sys
 
 try:
     # conecta com o banco
@@ -38,59 +39,62 @@ try:
 
     # lê arquivo de log
     try:
-        log = open('files/entradaLog')
+        log = open("files/entradaLog")
     except Exception as e:
         print(e)
         exit()
-    
-    # cria vetores pra armazenar transações que começaram e as que fizeram commit
+
+    # cria vetores pra armazenar as transações que começaram e as que fizeram commit
     started = []
     committed = []
 
-    # percorre log de trás pra frente fazendo undo em transações não comitadas
+    # percorre log de trás pra frente
     for linha in reversed(list(log)):
-        
-        #se chegar em um fim de checkpoint, não precisa mais olhar
-        if 'END CKPT' in linha:
+        # se chegar em um fim de checkpoint, não precisa mais olhar
+        if "END CKPT" in linha:
             break
 
-        # se for um commit 
-        if 'commit' in linha:
+        # se for um commit
+        if "commit" in linha:
             # add id da transação no vetor committed
-            committed.append(linha.split(' ')[1].replace('>\n',''))
+            committed.append(linha.split(" ")[1].replace(">\n", ""))
 
-        # se for um start 
-        if 'start' in linha and 'CKPT' not in linha:
+        # se for um start
+        if "start" in linha and "CKPT" not in linha:
             # add id da transação no vetor started
-            started.append(linha.split(' ')[1].replace('>\n',''))
+            started.append(linha.split(" ")[1].replace(">\n", ""))
 
-        
         # se for um registro de transação
-        match = re.search('<T.*,.*,.*,.*>', linha)
+        match = re.search("<T.*,.*,.*,.*>", linha)
         if match:
-            # remove <>
-            match = match.group(0).replace('<','').replace('>','')
-            
-            # coleta dados
-            tupla = match.split(',')
+            # remove caracteres <>
+            match = match.group(0).replace("<", "").replace(">", "")
+
+            # transforma em vetor
+            tupla = match.split(",")
 
             # se a transação não estiver comitada, faz UNDO
             if tupla[0] not in committed:
-                
                 # busca valor atual no banco
-                cursor.execute(f'SELECT {tupla[2]}  FROM data WHERE id = {tupla[1]}')
+                cursor.execute(f"SELECT {tupla[2]}  FROM data WHERE id = {tupla[1]}")
                 valor_bd = cursor.fetchone()[0]
 
                 # se o valor no banco for diferente do log, faz update no banco
                 if str(valor_bd) != str(tupla[3]):
-                    cursor.execute(f"UPDATE data SET {tupla[2]}={tupla[3]} WHERE id={tupla[1]}")
-
+                    cursor.execute(
+                        f"UPDATE data SET {tupla[2]}={tupla[3]} WHERE id={tupla[1]}"
+                    )
+                    print(
+                        f"{tupla[0]}: alteração no BD (linha {tupla[1]}, coluna {tupla[2]}) de {valor_bd} para {tupla[3]}"
+                    )
 
     # percorre as transações começadas e printa as não comitadas
     for t in started:
         if t not in committed:
-            print(f'Transação {t} realizou UNDO')
+            print(f"Transação {t} realizou UNDO")
     conn.commit()
+    print("\nID\tA\tB")
+    cursor.copy_to(sys.stdout, "data", sep="\t")
 
 
 except (Exception, psycopg2.DatabaseError) as error:
